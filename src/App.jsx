cd4b2751ee2b0a11d5a22b1aabee807a20c36548
Spencer Cherry <scherry@empoweringparents.com>
2018-02-17 14:19:42 -0500

    Initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+'use strict';
+
+import React from 'react';
+
+export default class App extends React.Component {
+
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return <div>This is app.</div>;
+  }
+
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var webpack   = require('webpack');
+var path      = require('path');
+
+module.exports = [
+  {
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+
+    entry: path.join(__dirname, 'index.jsx'),
+
+    output: {
+      filename: 'music.min.js'
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: ['node_modules'],
+          use: [
+            'babel-loader'
+          ]
+        }
+      ]
+    },
+
+    stats: {
+      colors: true
+    }
+  }
+];
